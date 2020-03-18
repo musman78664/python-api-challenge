80d56bf0007186d20635af04d235d4db55865765
Mohammad Usman <60331423+musman78664@users.noreply.github.com>
2020-03-18 07:45:00 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -3,4 +3,6 @@ weather_api_key = "b8dd2b579866997adafd4e8a7b7a423b"
 
 
 # Google API Key
-g_key = "AIzaSyBARiWa75TDagmtVkNPa_wO6zN8pHINfao"
+g_key = "-------------------------------------"
+
+# I had to remove the g_key because received an email from google since its public.
